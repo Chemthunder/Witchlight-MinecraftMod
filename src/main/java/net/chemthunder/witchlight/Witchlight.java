40b15f0692de291c33a51984375dc76d09ec93c0
thoma <chemthunderisme@gmail.com>
2025-06-04 20:49:34 -0400

    a

--- src/main/java/net/chemthunder/witchlight/Witchlight.java
+++ src/main/java/net/chemthunder/witchlight/Witchlight.java
@@ -4,6 +4,8 @@ import net.chemthunder.witchlight.block.ModBlocks;
 import net.chemthunder.witchlight.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,4 +20,16 @@ ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		LOGGER.info("I cast furry upon vbinnie");
 	}
+
+
+
+
+
 }
+
+
+
+
+
+
+
--- src/main/java/net/chemthunder/witchlight/item/custom/WitchlightStoneItem.java
+++ src/main/java/net/chemthunder/witchlight/item/custom/WitchlightStoneItem.java
@@ -1,12 +1,20 @@
 package net.chemthunder.witchlight.item.custom;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Vector2d;
+import org.joml.Vector2f;
+import org.joml.Vector3d;
 
 
 public class WitchlightStoneItem extends Block {
@@ -23,14 +31,22 @@ public class WitchlightStoneItem extends Block {
 
         if (!world.isClient() && placer.age % 100 <= 0) {
 
-            if (pos.getX() >= 1 && pos.getX() <= 1 + 1 - 1 &&
-                    pos.getY() >= 1 && pos.getY() <= 1 + 1 - 1 &&
-                    pos.getZ() >= 1 && pos.getZ() <= 1 + 1 - 1) {
-                // Entity is within the area, apply effects here
+            ServerCommandSource silentSource = new ServerCommandSource(
+                    world.getServer(),                                // MinecraftServer
+                    Vec3d.ofCenter(pos),                             // Position
+                    new Vec2f(0,0),                             // Rotation
+                    world.getServer().getOverworld(),                 // World
+                    4,                                     // Permission level (e.g., 4 for ops)
+                    "silent_executor",                     // Name
+                    Text.literal("silent_executor"),       // Display name
+                    null,                                // Entity (null is okay)
+                    null                                   // Entity as Command Output
+            ).withSilent();                  // Suppress all output
 
-
-
-
+            try {
+                world.getServer().getCommandManager().getDispatcher().execute("damage @e[distance=50] 4 minecraft:magic", silentSource);
+            } catch (CommandSyntaxException e) {
+                throw new RuntimeException(e);
             }
         }
     }
